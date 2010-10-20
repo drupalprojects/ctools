a36dfd61af14e065cc10b168d55db3685b7072ac
Earl Miles <merlin@logrus.com>
2010-10-20 19:22:08 +0000

    Add detach event to modal close so that wysiwyg can detach the editor.

--- js/modal.js
+++ js/modal.js
@@ -382,6 +382,7 @@
       $('body').unbind( 'focus', modalEventHandler);
       $('body').unbind( 'keypress', modalEventHandler );
       $('.close').unbind('click', modalContentClose);
+      $(document).trigger('CToolsDetachBehaviors', $('#modalContent'));
 
       // Set our animation parameters and use them
       if ( animation == 'fadeIn' ) animation = 'fadeOut';
@@ -440,6 +441,7 @@
     $('body').unbind('focus', modalEventHandler);
     $('body').unbind('keypress', modalEventHandler);
     $('.close').unbind('click', modalContentClose);
+    $(document).trigger('CToolsDetachBehaviors', $('#modalContent'));
 
     // jQuery magic loop through the instances and run the animations or removal.
     content.each(function(){
